76a3ea60d851f5c1b12190c2a48f1cae9aa14e6a
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-10-27 15:59:42 -0600

    Add pyfq module with FQ file parsers

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
+from Cython.Build import cythonize
+from distutils.extension import Extension
+
+
+extensions = cythonize([
+    Extension("metacov.pyfq", ["metacov/pyfq.pyx"])
+])
 
 setup(
     name="MetaCov",
     use_scm_version=True,
     packages=find_packages(),
+    ext_modules=extensions,
     setup_requires=[
         'setuptools_scm',
+    ],
+    tests_require=[
+        'pytest'
     ],
     install_requires=[
         'Click',
