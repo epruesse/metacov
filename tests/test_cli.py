1772ecb5481f681a30e413654ac2972c87728553
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-11-03 14:55:18 -0600

    Test `scan` with all input combinations

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,9 +11,10 @@ runner = CliRunner()
 path = os.path.join(os.path.dirname(__file__), 'data')
 fq1 = os.path.join(path, 'ecoli_1K_1.fq.gz')
 fq2 = os.path.join(path, 'ecoli_1K_2.fq.gz')
+bam = os.path.join(path, 'bbmap.sorted.bam')
 
-
-def test_scan(tmpdir):
+@pytest.mark.parametrize('infile', [[fq1], [fq2], [fq1, fq2], [bam]])
+def test_scan(tmpdir, infile):
     with tmpdir.as_cwd():
-        result = runner.invoke(scan, [fq1, "out"])
+        result = runner.invoke(scan, infile + ["out"])
         assert result.exit_code == 0, "\n===\n" + result.output + "\n==="
